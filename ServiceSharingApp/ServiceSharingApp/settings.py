591059cfeeab60b6bbc08cdfcb1603a3b17979aa
haydarsinan <78043047+haydarsinan@users.noreply.github.com>
2021-11-28 15:09:10 +0300

    POSTGRESQL Settings Update

--- ServiceSharingApp/ServiceSharingApp/settings.py
+++ ServiceSharingApp/ServiceSharingApp/settings.py
@@ -73,14 +73,14 @@ WSGI_APPLICATION = 'ServiceSharingApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-#Docker Database Settings
+#Docker PostgreSQL Database Settings
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -92,17 +92,17 @@ DATABASES = {
 #     }
 # }
 
-# #Local Database Settings
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': 5432
-#     }
-# }
+#Local PostgreSQL Database Settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '1175103',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
